8e5446cc09dfc2b6fdba0c9f687076cdaae49bc0
wuyinengyan <liuhengjuncn@sina.com>
2019-12-05 15:02:52 +0800

    3.迁移(python manage.py makemigrattions)，生成数据库表(python manage.py migrate)

--- user/models.py
+++ user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.CharField(max_length=20, null=False, verbose_name="用户名")
+    password = models.CharField(max_length=128, null=False, verbose_name="密码")
+    create_datetime = models.DateTimeField(verbose_name="创建时间")
+
+    class Meta:
+        db_table = "user"
+
+    def __repr__(self):
+        return f"{self.id}/{self.name}"
+
+    __str__ = __repr__
